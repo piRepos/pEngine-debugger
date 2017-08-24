b336611c36df6d35193f9b58dccfc79ddab00190
AndreaDemontis <pk.demontis@gmail.com>
2017-08-24 02:38:43 +0200

    Settings forgot commit.

--- app/components/program-settings.js
+++ app/components/program-settings.js
@@ -6,15 +6,54 @@ export default Ember.Component.extend(
 	classNames: ["program-settings"],
 
 	window: Ember.inject.service('window'),
+	pEngine: Ember.inject.service('pengine'),
 
-	//open: Ember.computed('window', function() 
-	//{
-	//	return this.get("window").settings.open;
-	//}),
+	// - General settings
+	refreshRate: 0,
+	closeEngine: false,
+	disableAudio: false,
+	
+	// - Connection settings
+	ip: "",
+	port: 0,
+	batching: false,
+
+
+	setup: Ember.observer("window.settings.open", function()
+	{
+		var pEngine = this.get("pEngine");
+
+		this.setProperties(
+		{
+			refreshRate: pEngine.refreshRate,
+			ip: pEngine.endpointIp,
+			port: pEngine.endpointPort
+		});
+
+    }),
 
 	actions:
 	{
 
+		save: function () 
+		{
+			var window = this.get("window");
+
+			this.setProperties(
+			{
+				"pEngine.refreshRate": this.get("refreshRate"),
+				"pEngine.endpointIp": this.get("ip"),
+				"pEngine.endpointPort": this.get("port")
+			});
+
+			window.toggleSettings();
+		},
+
+		close: function () 
+		{
+			var window = this.get("window");
+			window.toggleSettings();
+		}
 	}
 
 });
